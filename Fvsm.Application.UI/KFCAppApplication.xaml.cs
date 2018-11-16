a02bd5c471f4f6cc445813ed18b6d7c3540f7b6f
krizsipeti <krizsipeti@freemail.hu>
2018-11-16 01:34:34 +0100

    Initial check in

---
+++ Fvsm.Application.UI/KFCAppApplication.xaml.cs
@@ -0,0 +1,18 @@
+﻿//----------------------------------------------------------------------------
+// Project    :   FFmpegVsSlnMaker
+// Module     :   Fvsm.Application.UI
+// File       :   KFCAppApplication.xaml.cs
+//----------------------------------------------------------------------------
+// Revisions  :
+// 16-11-2018 Krizsán Péter             - Original version
+//----------------------------------------------------------------------------
+
+namespace Fvsm.Application.UI
+{
+    /// <summary>
+    /// Interaction logic for KFCAppApplication.xaml
+    /// </summary>
+    public partial class KFCAppApplication : System.Windows.Application
+    {
+    }
+}
---
+++ Fvsm.Application.UI/KFCAppMainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿//----------------------------------------------------------------------------
+// Project    :   FFmpegVsSlnMaker
+// Module     :   Fvsm.Application.UI
+// File       :   KFCAppMainWindow.xaml.cs
+//----------------------------------------------------------------------------
+// Revisions  :
+// 16-11-2018 Krizsán Péter             - Original version
+//----------------------------------------------------------------------------
+using System.Windows;
+
+namespace Fvsm.Application.UI
+{
+    /// <summary>
+    /// Interaction logic for KFCAppMainWindow.xaml
+    /// </summary>
+    public partial class KFCAppMainWindow : Window
+    {
+        /// <summary>
+        /// Class responsible for the main window.
+        /// </summary>
+        public KFCAppMainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
