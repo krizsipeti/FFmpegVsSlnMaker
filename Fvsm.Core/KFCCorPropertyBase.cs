5771d126e5838f794305a810b970eb4393ef111d
krizsipeti <krizsipeti@freemail.hu>
2018-11-16 13:17:21 +0100

    Initial core project check in

---
+++ Fvsm.Core/KFCCorPropertyBase.cs
@@ -0,0 +1,24 @@
+﻿//----------------------------------------------------------------------------
+// Project    :   FFmpegVsSlnMaker
+// Module     :   Fvsm.Core
+// File       :   KFCCorPropertyBase.cs
+//----------------------------------------------------------------------------
+// Revisions  :
+// 16-11-2018 Krizsán Péter             - Original version
+//----------------------------------------------------------------------------
+using System.ComponentModel;
+
+namespace Fvsm.Core
+{
+    /// <summary>
+    /// The base class of the bindable objects. Helps to
+    /// implement the Model-View-ViewModel (MVVM) architecture.
+    /// </summary>
+    public class KFCCorPropertyBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// By subscribing to this event the client receives notifications about property changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
